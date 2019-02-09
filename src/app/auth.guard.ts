cff651223177b9c5868d8082fefa9edcc9cc08f5
KDragon <elkarouani@gmail.com>
2019-02-09 22:41:09 +0100

    Task: make the authentication guard on work

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanActivate {
+  	
+  	constructor(private auth: AuthService) {
+
+  	}
+
+  	canActivate(
+  		next: ActivatedRouteSnapshot,
+  		state: RouterStateSnapshot
+  	): Observable<boolean> | Promise<boolean> | boolean {
+		return this.auth.isLoggedIn;
+	}
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface myData {
+	success: boolean,
+	secret: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+	private loggedInStatus = false;
+
+	setLoggedIn(value: boolean) {
+		this.loggedInStatus = value;
+	}
+
+	get isLoggedIn() {
+		return this.loggedInStatus;
+	}
+
   constructor(private http: HttpClient) { }
 
   getUserDetails(username, password) {
-	return this.http.post('http://localhost:1234/api/auth.php', {
+	return this.http.post<myData>('http://localhost:1234/api/auth.php', {
 		username,
 		password
 	});
